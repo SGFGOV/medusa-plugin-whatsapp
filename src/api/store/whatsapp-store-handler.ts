3b845d7e7f21de96558b8c7a160d6240188cb62b
SGF <SGF@sourcegoodfood.com>
2023-02-09 14:00:19 +0000

    fix: dummy routes

--- src/api/store/whatsapp-store-handler.ts
+++ src/api/store/whatsapp-store-handler.ts
@@ -3,14 +3,14 @@ import * as bodyParser from "body-parser";
 
 export default (): Router => {
   const app = Router();
-  app.use(bodyParser.json());
+  /*app.use(bodyParser.json());
 
   app.get(
     "/whatsapp-message",
     async (req: Request, res: Response): Promise<Response<void>> => {
       return res.status(400).json({});
     }
-  );
+  );*/
 
   return app;
 };
