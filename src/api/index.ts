cad6bf270f20d3f95f809d7d4c63f01c17045310
SGF <SGF@sourcegoodfood.com>
2023-02-13 09:19:42 +0000

    fix: config modules

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import whatsAppReceiverRoute from "./routes";
 import { WhatsappInterfaceOptions } from "../services/whatsapp-interface";
+import { ConfigModule } from "@medusajs/medusa/dist/types/global";
+import { getConfigFile } from "medusa-core-utils";
 /* TODO second argument pluginConfig: Record<string, unknown> part of PR https://github.com/medusajs/medusa/pull/959 not yet in master */
 export default (
   rootDirectory: string,
@@ -8,6 +10,11 @@ export default (
   config
 ): Router => {
   const whatsAppRouter = Router();
+  if (!config) {
+    /** to support standard @medusajs/medusa */
+    const { configModule } = getConfigFile(rootDirectory, "medusa-config");
+    config = configModule as ConfigModule;
+  }
 
   whatsAppReceiverRoute(whatsAppRouter, options, config);
 
