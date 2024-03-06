4ccb034b5f57c0671dfee21c0816f66de213ae3f
Govind Diwakar <sgf@sourcegoodfood.com>
2024-03-06 03:12:18 +0000

    fix: typings

--- src/api/routes/whastapp-route-handler.ts
+++ src/api/routes/whastapp-route-handler.ts
@@ -44,7 +44,7 @@ export default async (
       } else {
         activeSession.messages.push({
           sender: "USER",
-          message: "whatsappMessage",
+          message: whatsappMessage,
         });
       }
     }
